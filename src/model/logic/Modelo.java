a2812f48e9ea303c77c6a930c443b2eb2fc3db5e
Esteban Emmanuel Ortiz Morales <ee.ortiz@sis.virtual.uniandes.edu.co>
2020-02-28 18:24:37 -0500

    fd

--- src/model/logic/Modelo.java
+++ src/model/logic/Modelo.java
@@ -4,6 +4,8 @@ import model.data_structures.ArregloDinamico;
 import model.data_structures.Comparendo;
 import model.data_structures.IArregloDinamico;
 
+import java.util.Random;
+
 /**
  * Definicion del modelo del mundo
  *
@@ -61,4 +63,181 @@ public class Modelo {
 		return comps;
 	}
 
+	public String RetornarDatos(Comparendo comp){
+		//INFRACCION, OBJECTID,
+		//FECHA_HORA, CLASE_VEHI, TIPO_SERVI, LOCALIDAD.
+		String rta = "Codigo de infraccion: "+comp.INFRACCION +" ObjectID: " + comp.OBJECTID + " Fecha y hora: " + comp.FECHA_HORA + " Clase de vehiculo "+comp.CLASE_VEHI + " Tipo de servicio: " +
+				comp.TIPO_SERVI + " Localidad: "+ comp.LOCALIDAD;
+		return rta;
+	}
+
+	public Comparable[ ] copiarComparendos(){
+
+		Comparable[] rta = new Comparable[comps.darTamano()];
+		int i = 0;
+		while(i < comps.darTamano()){
+			rta[i] = comps.darElemento(i);
+			i++;
+		}
+
+		return rta;
+
+	}
+
+	// solucion adaptada de las presentaciones de sicua
+	public void shellSort(Comparable datos[]){
+
+		int tamano = datos.length;
+		int h = 1;
+		while (h < tamano/3){
+
+			h = 3*h + 1; // 1, 4, 13, 40, 121, 364, ...
+
+		}
+		while (h >= 1)
+		{ // h-sort the array.
+			for (int i = h; i < tamano; i++)
+			{
+				for (int j = i; j >= h && less(datos[j], datos[j-h]); j -= h){
+
+					exch(datos, j, j-h);
+				}
+
+			}
+			h = h/3;
+		}
+
+	}
+
+	/* This function takes last element as pivot, 
+    places the pivot element at its correct 
+    position in sorted array, and places all 
+    smaller (smaller than pivot) to left of 
+    pivot and all greater elements to right 
+    of pivot */
+
+	// solucion adaptada de: https://www.geeksforgeeks.org/quick-sort/
+	public static int partition(Comparable datos[], int low, int high) 
+	{ 
+		Comparable pivote = datos[high];  
+		int i = (low-1); // index of smaller element 
+		for (int j=low; j<high; j++) 
+		{ 
+			// Si el elemento actual es menor que el pivote
+			if (less(datos[j], pivote)) 
+			{ 
+				i++; 
+				// swap datos[i] and datos[j]  
+				exch(datos, i, j);
+			} 
+		} 
+
+		// swap datos[i+1] and datos[high] (o pivote) 
+		exch(datos, i+1, high);
+
+		return i+1; 
+	} 
+
+	//relacionado al quicksort, de aqui inicia.
+	public static void sort(Comparable[] a)
+	{
+		StdRandom.shuffle(a);
+		sort(a, 0, a.length - 1);
+	} 
+	/* The main function that implements QuickSort() 
+   datos[] --> Array to be sorted, 
+   low  --> Starting index, 
+   high  --> Ending index */
+
+	// solucion adaptada de: https://www.geeksforgeeks.org/quick-sort/
+	public static void sort(Comparable datos[], int low, int high) 
+	{ 
+		if (low < high) 
+		{ 
+			/* pi is partitioning index, arr[pi] is  
+           now at right place */
+			int pi = partition(datos, low, high); 
+
+			// Recursively sort elements before 
+			// partition and after partition 
+			sort(datos, low, pi-1); 
+			sort(datos, pi+1, high); 
+		} 
+	} 
+
+	public static void exch(Comparable[] a, int i, int j ){
+
+		Comparable temporal = a[i];
+		a[i] = a[j];
+		a[j] = temporal;
+	}
+
+	public static boolean less(Comparable a, Comparable b){
+
+		if(a.compareTo(b)<0){
+			return true;
+		}
+		else{
+			return false; //mayor o igual a cero
+		}
+	}
+	/*
+	 * paso 2 algoritmo mergeSort
+	 */
+	private static void sortParaMergeSort(Comparable[] a, Comparable[] aux, int lo, int hi)
+	{
+		if (hi <= lo) return;
+		int mid = lo + (hi - lo) / 2;
+		sortParaMergeSort(a, aux, lo, mid);
+		sortParaMergeSort(a, aux, mid+1, hi);
+		merge(a, aux, lo, mid, hi);
+	}
+
+	/*
+	 *  aqui inicia el algoritmo mergeSort sacado del libro algorithms 4 edicion
+	 */
+	public static void sortParaMerge(Comparable[] a)
+	{
+		Comparable[] aux = new Comparable[a.length];
+		sortParaMergeSort(a, aux, 0, a.length - 1);
+	}
+
+
+
+	/*
+	 * ultimo paso
+	 */
+	private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi)
+	{
+		for (int k = lo; k <= hi; k++)
+			aux[k] = a[k];
+		int i = lo, j = mid+1;
+		for (int k = lo; k <= hi; k++)
+		{
+			if (i > mid) a[k] = aux[j++];
+			else if (j > hi) a[k] = aux[i++];
+			else if (less(aux[j], aux[i])) //si izquierda menor true
+			{ a[k] = aux[j++];}
+			else a[k] = aux[i++];
+		}
+	} 
+
+	/** primer requerimiento estudiante A
+	 * 1A- Consultar el primer comparendo que aparezca en el archivo que tiene una LOCALIDAD
+	 * dada. Mostrar el resultado indicando para el comparendo OBJECTID, FECHA_HORA,
+	 *	 
+	 *	 INFRACCION, CLASE_VEHI, TIPO_SERVI, LOCALIDAD. Reportar el caso especial en que NO
+	 * exista información al respecto. 
+	 */
+	public String requerimiento1EstudianteA(String Plocalidad ){
+
+		int i = 0;
+		boolean encontrado = false;
+		while(i<comps.length && encontrado ==false){
+
+			
+		}
+	}
+
 }
+
